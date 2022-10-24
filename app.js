efdda5197f45593b596550807c873d7b43762844
FaheerHakim <102043613+FaheerHakim@users.noreply.github.com>
2022-10-24 17:04:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 function doSomething() {
  // do something
     console.log('doSomething');
-    doSomethingElse();
+ 
 }
 function doSomethingElse() {
  // do something else
     console.log('ok');
 }
 doSomething();
+ doSomethingElse();
