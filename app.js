271ce0116fa25b8329f8b9cb90295fbdf0742414
FaheerHakim <r0820158@student.thomasmore.be>
2022-10-24 16:55:59 +0200

    added somethingElse to app.js

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 function doSomething() {
  // do something
     console.log('doSomething');
+    doSomethingElse();
+}
+function doSomethingElse() {
+ // do something else
+    console.log('ok');
 }
 doSomething();
